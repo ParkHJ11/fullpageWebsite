5f04ea35157ca35f31beece0631879414eef0e52
phj <parkhj8801@gmail.com>
2020-10-16 19:47:02 +0900

    메인페이지 html작성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
+  // Main Page Fullpage
   $('#fullpage').fullpage({
-    //options here
-    // autoScrolling: true,
-    anchors: ['firstPage', 'secondPage', 'thirdPage'],
   });
+
+  // Main Page Slide
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
 });

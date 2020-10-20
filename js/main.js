1eeb20f3ecb0fd128e23c80c8b3fb73a167fb712
phj <parkhj8801@gmail.com>
2020-10-20 21:10:25 +0900

    페이저, 섹션이동버튼 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
   // Main Page Fullpage
   $('#fullpage').fullpage({
+    afterLoad: function (origin, destination, direction) {
+      // using index
+      // destination의 인덱스번호가 0보다 크면(첫페이지가 아니면) 풀페이지nav를 표시
+      if (destination.index > 0) {
+        $('.fixed-nav').fadeIn();
+      } else {
+        $('.fixed-nav').fadeOut();
+      }
+
+      $('.fixed-nav').find('a').removeClass('active');
+      $('.fixed-nav').find('a').eq(destination.index).addClass('active');
+    },
   });
 
   // Main Page Slide

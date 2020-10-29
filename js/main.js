316d1fb39e5c75ad7d7b63cefa420e9e49312037
phj <parkhj8801@gmail.com>
2020-10-29 20:52:40 +0900

    html 구조변경전

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@ $(document).ready(function () {
       // destination의 인덱스번호가 0보다 크면(첫페이지가 아니면) 풀페이지nav를 표시
       if (destination.index > 0) {
         $('.fixed-nav').fadeIn();
+        if (destination.index == 1 || destination.index == 3) {
+          $('.fixed-nav').find('a').removeClass('active');
+          $('.fixed-nav').find('a').eq(destination.index).addClass('active-black');
+        } else {
+          $('.fixed-nav').find('a').removeClass('active-black');
+          $('.fixed-nav').find('a').eq(destination.index).addClass('active');
+        }
       } else {
         $('.fixed-nav').fadeOut();
       }
-
-      $('.fixed-nav').find('a').removeClass('active');
-      $('.fixed-nav').find('a').eq(destination.index).addClass('active');
     },
   });
 

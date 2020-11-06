f1c81bf002c91a86e508efc5a4e7a2b4130a32e3
phj <parkhj8801@gmail.com>
2020-11-07 01:21:00 +0900

    subpage 추가

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
 $(document).ready(function () {
   // Main Page Fullpage
-  $('#fullpage').fullpage({
-    afterLoad: function (origin, destination, direction) {
-      // using index
-      // destination의 인덱스번호가 0보다 크면(첫페이지가 아니면) 풀페이지nav를 표시
-      if (destination.index > 0) {
-        $('.fixed-nav').fadeIn();
-        $('.header nav').addClass('fixed');
-        $('.logo img').attr('src', 'images/images_finish/logoImage.png');
+  // #fullpage가 있으면
+  if ($('#fullpage').length) {
+    $('#fullpage').fullpage({
+      afterLoad: function (origin, destination, direction) {
+        // using index
+        // destination의 인덱스번호가 0보다 크면(첫페이지가 아니면) 풀페이지nav를 표시
+        if (destination.index > 0) {
+          $('.fixed-nav').fadeIn();
+          $('.header nav').addClass('fixed');
+          $('.logo img').attr('src', 'images/images_finish/logoImage.png');
 
-        if (destination.index == 1 || destination.index == 3) {
-          $('.fixed-nav').find('a').removeClass('active');
-          $('.fixed-nav').find('a').eq(destination.index).addClass('active-black');
+          if (destination.index == 1 || destination.index == 3) {
+            $('.fixed-nav').find('a').removeClass('active');
+            $('.fixed-nav').find('a').eq(destination.index).addClass('active-black');
+          } else {
+            $('.fixed-nav').find('a').removeClass('active-black');
+            $('.fixed-nav').find('a').eq(destination.index).addClass('active');
+          }
         } else {
-          $('.fixed-nav').find('a').removeClass('active-black');
-          $('.fixed-nav').find('a').eq(destination.index).addClass('active');
+          $('.fixed-nav').fadeOut();
+          $('.header nav').removeClass('fixed');
+          $('.logo img').attr('src', 'images/images_finish/logo_white.png');
         }
-      } else {
-        $('.fixed-nav').fadeOut();
-        $('.header nav').removeClass('fixed');
-        $('.logo img').attr('src', 'images/images_finish/logo_white.png');
-      }
-      if (destination.index == 5) {
-        $('.back_to_top').addClass('active');
-      } else {
-        $('.back_to_top').removeClass('active');
-      }
-    },
-  });
+        if (destination.index == 5) {
+          $('.back_to_top').addClass('active');
+        } else {
+          $('.back_to_top').removeClass('active');
+        }
+      },
+    });
+  } // fullpage end
 
   // Main Page Slide
-  var mySwiper = new Swiper('.swiper-container', {
+  var mySwiper = new Swiper('.main-slides', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,

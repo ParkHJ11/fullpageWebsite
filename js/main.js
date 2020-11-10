87c2ae39732fb8d8e0806c7764116db1b3e9cd46
phj <parkhj8801@gmail.com>
2020-11-10 16:06:44 +0900

    about page 완성

--- js/main.js
+++ js/main.js
@@ -33,23 +33,34 @@ $(document).ready(function () {
   } // fullpage end
 
   // Main Page Slide
-  var mySwiper = new Swiper('.main-slides', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  if ($('.main-slides').length > 0) {
+    var mySwiper = new Swiper('.main-slides', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } // main slides
+  if ($('.testimonial-slides').length > 0) {
+    var testimonialSwiper = new Swiper('.testimonial-slides', {
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } // testimonial-slides
 
   // toggle Menu Open & close
   $('.nav-button').click(function () {
@@ -68,5 +79,21 @@ $(document).ready(function () {
       $('.subpage nav').removeClass('fixed');
       $('.logo img').attr('src', '../images/images_finish/logo_white.png');
     }
+
+    // back to top by scrollTop
+    // 푸터가 상단에서 떨어진거리 - 800
+    var footerOST = $('.footer').offset().top - 800;
+    // 보다 스크롤 양이 많으면
+    if ($(this).scrollTop() > footerOST) {
+      $('.back_to_top').addClass('active');
+    } else {
+      $('.back_to_top').removeClass('active');
+    }
+  });
+
+  // .about_btt를 클릭하면 화면상단 이동
+  $('.about_btt').click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 500, 'easeOutQuint');
   });
 }); // document ready
